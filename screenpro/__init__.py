bfaf7272bc74681768aab4eb2b2bd721c504e6a8
abearab <abardesigner@gmail.com>
2023-08-23 19:53:39 -0700

    minor update

--- screenpro/__init__.py
+++ screenpro/__init__.py
@@ -9,13 +9,15 @@ from scipy.stats import ttest_rel
 def runPhenoScoreByReps(adata, cond1, cond2, growth_rate=1, n_reps=2, ctrl_label='negCtrl', test='ttest'):
     """Calculate phenotype score and p-values comparing `cond2` vs `cond1`
     """
+    print(f'\t{cond2} vs {cond1}')
+
     count_layer = 'seq_depth_norm'
     # check if count_layer exists
     if 'seq_depth_norm' not in adata.layers.keys():
         seqDepthNormalization(adata)
     # prep counts for phenoScore calculation
-    df_cond1 = adata[adata.obs.query(f'condition=="{cond1}"').index[:n_reps],].to_df(count_layer).T
-    df_cond2 = adata[adata.obs.query(f'condition=="{cond2}"').index[:n_reps],].to_df(count_layer).T
+    df_cond1 = adata[adata.obs.query(f'condition=="{cond1}"').index[:n_reps], ].to_df(count_layer).T
+    df_cond2 = adata[adata.obs.query(f'condition=="{cond2}"').index[:n_reps], ].to_df(count_layer).T
 
     x = df_cond1.to_numpy()
     y = df_cond2.to_numpy()
