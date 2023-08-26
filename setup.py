a2c68411ab52758db56d8ab338c7251de0182f1b
abearab <abardesigner@gmail.com>
2023-08-25 18:13:42 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 from screenpro import __version__
+from pathlib import Path
+
+this_directory = Path(__file__)
+long_description = (this_directory / "README.rst").read_text()
 
 setup(
     python_requires='>=3.8',
@@ -19,5 +23,8 @@ setup(
     #     "anndata>=0.8.0",
     #     "pydeseq2"
     # ],
-    version=__version__
+    version=__version__,
+    # other arguments omitted
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
 )
