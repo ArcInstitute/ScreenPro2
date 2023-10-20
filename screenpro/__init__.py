1df2f989153882fd68e5ec594402ae86babca3bc
abearab <abardesigner@gmail.com>
2023-10-20 15:26:29 -0700

    add copy method

--- screenpro/__init__.py
+++ screenpro/__init__.py
@@ -2,6 +2,7 @@ import pandas as pd
 from .__version__ import __version__
 from .phenoScore import seqDepthNormalization, matrixStat, matrixTest
 from .phenoScore import runPhenoScore
+from copy import copy
 
 
 class ScreenPro(object):
@@ -21,6 +22,9 @@ class ScreenPro(object):
             descriptions += f"Phenotypes in score_level = '{score_level}':\n    scores: '{scores}'\n"
 
         return f'obs->samples\nvar->oligos\n\n{self.adata.__repr__()}\n\n{descriptions}'
+
+    def copy(self):
+        return copy(self)
 
     def calculateDrugScreen(self,
                             t0, untreated, treated, growth_rate,
