96ffae17a93341ac87092018acd2874738b564f7
Nick Youngblut <nick.youngblut@arcinstitute.org>
2023-11-08 04:48:40 +0000

    updated setup.py

---
+++ README.md
@@ -0,0 +1,7 @@
+![image](https://img.shields.io/pypi/v/screenpro2.svg) [![Documentation Status](https://readthedocs.org/projects/screenpro2/badge/?version=latest)](https://screenpro2.readthedocs.io/en/latest/?version=latest) ![Downloads](https://static.pepy.tech/badge/screenpro2)
+
+
+ScreenPro2
+==========
+
+The docs are available at [https://screenpro2.readthedocs.io](https://screenpro2.readthedocs.io)
--- setup.py
+++ setup.py
@@ -3,28 +3,23 @@ from screenpro import __version__
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     name='ScreenPro2',
     description="Analyze pooled CRISPR screens",
-    packages=find_packages(include=['screenpro', 'screenpro.*']),
-    # install_requires=[
-    #     "anndata",
-    #     "numpy",
-    #     "pandas",
-    #     "scipy",
-    #     "biopython",
-    #     "matplotlib",
-    #     "seaborn",
-    #     "pandas",
-    #     "numpy",
-    #     "anndata>=0.8.0",
-    #     "pydeseq2"
-    # ],
-    version=__version__,
-    # other arguments omitted
     long_description=long_description,
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/markdown',
+    author='Abe Arab',
+    author_email='abea@arcinstitute.org',
+    url='https://github.com/abearab/ScreenPro2',
+    packages=find_packages(include=['screenpro', 'screenpro.*']),
+    version=__version__,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ]
 )
