730b263a712d162a3993e852b14db5955e0537f3
abearab <abardesigner@gmail.com>
2023-12-04 14:22:47 -0800

    update sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@ import screenpro
 # -- General configuration ---------------------------------------------
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
