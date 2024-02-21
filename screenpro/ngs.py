bc5c218849170177577e1371c585d3fc2305f761
Abolfazl (Abe) <abea@arcinstitute.org>
2024-02-21 01:46:36 -0800

    merge `trim_ngs_reads` functionality within count

--- screenpro/ngs.py
+++ screenpro/ngs.py
@@ -34,34 +34,16 @@ def fastq_to_dataframe(fastq_file_path: str,n_bp_from_5p=0, n_bp_from_3p=0) -> p
     return df
 
 
-def trim_ngs_reads(df: pl.DataFrame, n_bp_from_5p: int, n_bp_from_3p: int) -> pl.DataFrame:
-    """
-    Trims the sequences in a Polars DataFrame
-
-    Parameters
-    ----------
-    df : pl.DataFrame
-        The Polars DataFrame containing the sequences
-    n_bp_from_5p : int
-        The number of base pairs to trim from the 5' end
-    n_bp_from_3p : int
-        The number of base pairs to trim from the 3' end
-    """
-    t0 = time()
-    print('Trim sequences')
-
-    df['sequence'] = df['sequence'].str.slice(n_bp_from_5p, -n_bp_from_3p)
-
-    print("done in %0.3fs" % (time() - t0))
-
-    return df
-
-
-def fastq_to_count_unique_seq(fastq_file_path: str) -> pl.DataFrame:
+def fastq_to_count_unique_seq(fastq_file_path: str, n_bp_from_5p= None, n_bp_from_3p=None) -> pl.DataFrame:
     df = fastq_to_dataframe(fastq_file_path)
 
     t0 = time()
     print('Count unique sequences')
+
+    if n_bp_from_5p is not None:
+        df['sequence'] = df['sequence'].str.slice(n_bp_from_5p)
+    if n_bp_from_3p is not None:
+        df['sequence'] = df['sequence'].str.slice(-n_bp_from_3p)
 
     df_count = df.groupby('sequence').count()
 
