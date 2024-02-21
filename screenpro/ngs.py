9180855221b53fff993a02dac74e3ba05c9ee01e
Abolfazl (Abe) <abea@arcinstitute.org>
2024-02-20 23:58:31 -0800

    avoid biopython

--- screenpro/ngs.py
+++ screenpro/ngs.py
@@ -2,32 +2,11 @@
 '''
 
 import gzip
-from Bio import SeqIO
 from time import time
 import pandas as pd
 import polars as pl
-from joblib import Parallel, delayed
-
-
-def read_records(file_path, seq_only=True):
-    if file_path.endswith('.gz'):
-        handle = gzip.open(file_path, "rt")
-    else:
-        handle = open(file_path, "rt")
-
-    records = []
-    for record in SeqIO.parse(handle, "fastq"):
-        if seq_only:
-            records.append((str(record.seq)))
-        else:
-            records.append((str(record.id), str(record.seq), str(record.letter_annotations["phred_quality"])))
-
-    handle.close()
-
-    return records
-
-
 import concurrent.futures
+
 
 def fastq_to_dataframe(fastq_file_path: str, num_threads: int, seq_only=True) -> pl.DataFrame:
     """
@@ -39,24 +18,23 @@ def fastq_to_dataframe(fastq_file_path: str, num_threads: int, seq_only=True) ->
     t0 = time()
     print('load FASTQ file as a Polars DataFrame')
 
-    # Read the FASTQ file using read_records function
-    records = read_records(fastq_file_path, seq_only)
+    # TODO: Add support using biobear
+    # # Read the FASTQ file using read_records function
+    # records = read_records(fastq_file_path, seq_only)
 
-    # Create a Polars DataFrame from the list of tuples
-    if seq_only:
-        df = pd.DataFrame(records, columns=['seq'], dtype='str')
-    else:
-        df = pd.DataFrame(records, columns=['id', 'seq', 'qual'], dtype='str')
-    df = pl.from_pandas(df)
+    # # Create a Polars DataFrame from the list of tuples
+    # if seq_only:
+    #     df = pl.Series(records,dtype = pl.String).lazy()
+    #     # df.columns = ['seq']
+    # else:
+    #     df = pl.DataFrame(records).lazy()
+    #     df.columns = ['id', 'seq', 'qual']
+    #     df.dtypes = pl.String
+
 
     print("done in %0.3fs" % (time() - t0))
 
-    # Enable parallel execution using multiple threads
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        df = df.with_workers(num_threads)
-        df = df.lazy()
-
-    return df
+    # return df
 
 
 def fastq_to_count_unique_seq(fastq_file_path: str, num_threads: int) -> pl.DataFrame:
