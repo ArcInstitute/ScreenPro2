a92b6f49fb27fcf6d39c48986d93975ebef8c7db
abearab <abea@arcinstitute.org>
2024-03-02 06:46:53 +0000

    clean up for merge

--- screenpro/ngs.py
+++ screenpro/ngs.py
@@ -67,18 +67,3 @@ def map_sample_counts_to_library(library, sample):
     counts_df.loc[overlap, 'counts'] = sample.set_index('sequence').loc[overlap, 'count']
 
     return counts_df
-
-
-@click.command()
-@click.argument('fastq_file_path', type=click.Path(exists=True))
-@click.option('--engine', default='biopython', help='Engine to use for reading FASTQ file')
-@click.option('--slice-seq', nargs=2, type=int, help='Slice sequence range')
-def main(fastq_file_path, engine, slice_seq):
-    """
-    Command line interface for working with NGS data.
-    """
-    fastq_to_count_unique_seq(fastq_file_path, engine=engine, slice_seq=slice_seq)
-
-
-if __name__ == '__main__':
-    main()
