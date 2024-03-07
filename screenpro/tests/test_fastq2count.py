506bfd2654199bc0f399b9429083e56128b2c9c9
abearab <abardesigner@gmail.com>
2024-03-06 19:28:46 -0800

    minor fixes

--- screenpro/tests/test_fastq2count.py
+++ screenpro/tests/test_fastq2count.py
@@ -1,22 +1,27 @@
-from .screenpro import ngs
+import polars as pl
+from screenpro import ngs
 
 
 def test_single_guide():
-    ngs.fastq_to_count_single_guide(
+    df_count = ngs.fastq_to_count_single_guide(
         fastq_file_path='demo/step1_process_fastq_files/example_crispri_v2_sample.fastq',
         trim5p_start=2,
         trim5p_length=19,
         verbose=True
     )
 
+    assert isinstance(df_count, pl.DataFrame)
+
 
 def test_dual_guide():
-    ngs.fastq_to_count_dual_guide(
-        R1_fastq_file_path='demo/step1_process_fastq_files/example_crispri_v3_sample_R1.fastq',
-        R2_fastq_file_path='demo/step1_process_fastq_files/example_crispri_v3_sample_R2.fastq',
+    df_count = ngs.fastq_to_count_dual_guide(
+        R1_fastq_file_path='demo/step1_process_fastq_files/example_crispri_v3_sample_R1.fastq.gz',
+        R2_fastq_file_path='demo/step1_process_fastq_files/example_crispri_v3_sample_R2.fastq.gz',
         trim5p_pos1_start=2,
         trim5p_pos1_length=19,
         trim5p_pos2_start=2,
         trim5p_pos2_length=19,
         verbose=True
     )
+
+    assert isinstance(df_count, pl.DataFrame)
