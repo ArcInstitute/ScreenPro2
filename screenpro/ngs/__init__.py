b796ae7f84fed18630e543d09301b508630776ec
Abolfazl (Abe) <abea@arcinstitute.org>
2024-04-06 00:35:22 -0700

    update docs

--- screenpro/ngs/__init__.py
+++ screenpro/ngs/__init__.py
@@ -10,6 +10,7 @@
 
 This module provides functions to process FASTQ files from screens with single or dual guide
 libraries. In general, the algorithm is fairly simple:
+
 1. Read the FASTQ file and extract the proper sequences
 2. Count the exact number of occurrences for each unique sequence
 3. Map the counted sequences to the reference sequence library
@@ -26,9 +27,9 @@ library of protospacer A and B pairs.
 Theoretically, the algorithm is able to detect any observed sequence since it is counting first
 and then mapping. Therefore, the recombination events can be detected. In dual-guide design
 protospacer A and B are not the same pairs as in the reference library. These events include:
-- Protospacer A and B pairs are present in the reference library but paired differently
-- Only one of the protospacer A and B is present in the reference library
-- None of the protospacer A and B is present in the reference library
+* Protospacer A and B pairs are present in the reference library but paired differently
+* Only one of the protospacer A and B is present in the reference library
+* None of the protospacer A and B is present in the reference library
 '''
 
 from . import cas9
