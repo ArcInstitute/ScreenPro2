41f4bd846738c738b9322ac291f0d633521936d4
Abolfazl (Abe) <abea@arcinstitute.org>
2024-04-15 22:35:36 -0700

    rename to `assays`

--- screenpro/__init__.py
+++ screenpro/__init__.py
@@ -2,7 +2,7 @@ from . import plotting as pl
 from . import phenoScore as ps
 from . import utils
 from . import ngs
-from .crisprscreens import PooledScreens, GImaps
+from .assays import PooledScreens, GImaps
 
 __version__ = "0.2.10"
 __author__ = "Abe Arab"
