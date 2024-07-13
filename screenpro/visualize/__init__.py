44d60dbacabd15d93f3080cf305dcef554f6d957
abearab <abea@arcinstitute.org>
2024-07-13 10:32:00 -0700

    mend

--- screenpro/visualize/__init__.py
+++ screenpro/visualize/__init__.py
@@ -5,49 +5,9 @@
 
 import numpy as np
 import scanpy as sc
-from .utils import almost_black
-
-
-## Scatter plot of replicates
-
-def plotReplicateScatter(ax, adat_in, x, y, title, min_val=None, max_val=None, log_transform=True, **args):
-    adat = adat_in[[x, y], :].copy()
-
-    adat.obs.index = [f'Replicate {str(r)}' for r in adat.obs.replicate.to_list()]
-    x_lab, y_lab = [f'Replicate {str(r)}' for r in adat.obs.replicate.to_list()]
-
-    if log_transform:
-        adat.X = np.log10(adat.X+1)
-    
-    if min_val is None:
-        min_val = min([adat.to_df().loc[x_lab,:].min(), adat.to_df().loc[y_lab,:].min()])
-        min_val = min_val * 1.1 
-    if max_val is None:
-        max_val = max([adat.to_df().loc[x_lab,:].max(), adat.to_df().loc[y_lab,:].max()])
-        max_val = max_val * 1.1
-    
-    sc.pl.scatter(
-        adat,
-        x_lab, y_lab,
-        legend_fontsize='xx-large',
-        palette=[almost_black, '#BFBFBF'],
-        color='targetType',
-        title=title,
-        size=5,
-        show=False,
-        ax=ax,
-        **args
-    )
-    ax.set_ylim(min_val, max_val)
-    ax.set_xlim(min_val, max_val)
-    ax.tick_params(axis='both', labelsize=10)
-    ax.get_legend().remove()
-
-    ax.grid(False)
-
+from .qc_plots import *
 
 ## Phenotype plotting functions
-
 class DrugScreenPlotter:
     def __init__(self, screen, treated, untreated, t0='T0', threshold=3, ctrl_label='negative_control',run_name='auto'):
         self.screen = screen
@@ -88,9 +48,9 @@ class DrugScreenPlotter:
                 ax.annotate(txt, (target_data[x_col].iloc[i] + t_x, target_data[y_col].iloc[i] + t_y),
                             color=textcolor, size=size_txt)
 
-    def _prep_data(self,screen, score_col='score', pvalue_col='pvalue'):
+    def _prep_data(self, score_col='score', pvalue_col='pvalue'):
 
-        gamma = screen.getPhenotypeScores(
+        gamma = self.screen.getPhenotypeScores(
             run_name=self.run_name,
             score_name=self.gamma_score_name,
             threshold=self.threshold,
