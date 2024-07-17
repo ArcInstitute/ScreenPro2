5cb38ddec80fce5b90173a647ed8e3007ba07994
abearab <abea@arcinstitute.org>
2024-07-16 19:54:39 -0700

    reorganize plotting features

--- screenpro/plotting/pheno_plots.py
+++ screenpro/plotting/pheno_plots.py
@@ -1,62 +1,20 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from .utils import yellow_blue
+from ._utils import yellow_blue
+from ._rank import rank_plot
 
 
-class ScatterPlot:
-    def __init__(
-            self, df, up_hit, down_hit, ax,
-            score_col, pvalue_col, xlabel, ylabel,
-            ctrl_label='negative_control',
-            dot_size=1,
-            xlims='auto',
-            ylims='auto',
-            **args
-            ):
-        if f'-log10({pvalue_col})' not in df.columns:
-            df[f'-log10({pvalue_col})'] = np.log10(df[pvalue_col]) * -1
-
-        if xlims == 'auto':
-            xlims = (df[score_col].min() - 0.1, df[score_col].max() + 0.1)
-        if ylims == 'auto':
-            ylims = (df[f'-log10({pvalue_col})'].min() - 0.1, df[f'-log10({pvalue_col})'].max() + 0.1)
-        
-        # Scatter plot for each category
-        ax.scatter( df.loc[df['label'] == 'target_non_hit', score_col],
-                    df.loc[df['label'] == 'target_non_hit', f'-log10({pvalue_col})'],
-                    alpha=0.1, s=dot_size, c='black', label='target_non_hit',
-                    **args)
-        
-        ax.scatter( df.loc[df['label'] == up_hit, score_col], 
-                    df.loc[df['label'] == up_hit, f'-log10({pvalue_col})'],
-                    alpha=0.9, s=dot_size, c='#fcae91', label=up_hit,
-                    **args)
-        
-        ax.scatter( df.loc[df['label'] == down_hit, score_col], 
-                    df.loc[df['label'] == down_hit, f'-log10({pvalue_col})'],
-                    alpha=0.9, s=dot_size, c='#bdd7e7', label=down_hit,
-                    **args)
-        
-        ax.scatter( df.loc[df['label'] == ctrl_label, score_col],
-                    df.loc[df['label'] == ctrl_label, f'-log10({pvalue_col})'],
-                    alpha=0.1, s=dot_size, c='gray', label=ctrl_label,
-                    **args)
-
-        # Set x-axis and y-axis labels
-        ax.set_xlabel(xlabel)
-        ax.set_ylabel(ylabel)
-
-        # Set x-axis limits
-        ax.set_xlim(xlims)
-
-        # Set y-axis limits
-        ax.set_ylim(ylims)
-
-        # Add legend
-        ax.legend()
-
-        return ax
+## Phenotype plotting functions
+class DrugScreenPlotter:
+    def __init__(self, screen, treated, untreated, t0='T0', threshold=3, ctrl_label='negative_control',run_name='auto'):
+        self.screen = screen
+        self.threshold = threshold
+        self.ctrl_label = ctrl_label
+        self.run_name = run_name
+        self.gamma_score_name = f'gamma:{untreated}_vs_{t0}'
+        self.rho_score_name = f'rho:{treated}_vs_{untreated}'
+        self.tau_score_name = f'tau:{treated}_vs_{t0}'
 
     def _label_by_color(self, ax, df_in, label, 
                         x_col, y_col,
@@ -88,6 +46,224 @@ class ScatterPlot:
                 ax.annotate(txt, (target_data[x_col].iloc[i] + t_x, target_data[y_col].iloc[i] + t_y),
                             color=textcolor, size=size_txt)
 
+    def _prep_data(self, score_col='score', pvalue_col='pvalue'):
+
+        gamma = self.screen.getPhenotypeScores(
+            run_name=self.run_name,
+            score_name=self.gamma_score_name,
+            threshold=self.threshold,
+            ctrl_label=self.ctrl_label,
+            score_col=score_col,
+            pvalue_col=pvalue_col
+        )
+
+        gamma[f'-log10({pvalue_col})'] = np.log10(gamma[pvalue_col]) * -1
+
+        tau = self.screen.getPhenotypeScores(
+            run_name=self.run_name,
+            score_name=self.tau_score_name,
+            threshold=self.threshold,
+            ctrl_label=self.ctrl_label,
+            score_col=score_col,
+            pvalue_col=pvalue_col
+        )
+        tau[f'-log10({pvalue_col})'] = np.log10(tau[pvalue_col]) * -1
+
+        rho = self.screen.getPhenotypeScores(
+            run_name=self.run_name,
+            score_name=self.rho_score_name,
+            threshold=self.threshold,
+            ctrl_label=self.ctrl_label,
+            score_col=score_col,
+            pvalue_col=pvalue_col
+        )
+        rho[f'-log10({pvalue_col})'] = np.log10(rho[pvalue_col]) * -1
+
+        return gamma, tau, rho
+    
+    def _volcano(
+            self, ax, df, up_hit, down_hit,
+            score_col='score', pvalue_col='pvalue',
+            xlabel='phenotype score',
+            ylabel='-log10(pvalue)',
+            dot_size=1,
+            xlims='auto',
+            ylims='auto',
+            **args
+            ):
+        
+        if f'-log10({pvalue_col})' not in df.columns:
+            df[f'-log10({pvalue_col})'] = np.log10(df[pvalue_col]) * -1
+
+        if xlims == 'auto':
+            xlims = (df[score_col].min() - 0.1, df[score_col].max() + 0.1)
+        if ylims == 'auto':
+            ylims = (df[f'-log10({pvalue_col})'].min() - 0.1, df[f'-log10({pvalue_col})'].max() + 0.1)
+        
+        # Scatter plot for each category
+        ax.scatter( df.loc[df['label'] == 'target_non_hit', score_col],
+                    df.loc[df['label'] == 'target_non_hit', f'-log10({pvalue_col})'],
+                    alpha=0.1, s=dot_size, c='black', label='target_non_hit',
+                    **args)
+        
+        ax.scatter( df.loc[df['label'] == up_hit, score_col], 
+                    df.loc[df['label'] == up_hit, f'-log10({pvalue_col})'],
+                    alpha=0.9, s=dot_size, c='#fcae91', label=up_hit,
+                    **args)
+        
+        ax.scatter( df.loc[df['label'] == down_hit, score_col], 
+                    df.loc[df['label'] == down_hit, f'-log10({pvalue_col})'],
+                    alpha=0.9, s=dot_size, c='#bdd7e7', label=down_hit,
+                    **args)
+        
+        ax.scatter( df.loc[df['label'] == self.ctrl_label, score_col],
+                    df.loc[df['label'] == self.ctrl_label, f'-log10({pvalue_col})'],
+                    alpha=0.1, s=dot_size, c='gray', label=self.ctrl_label,
+                    **args)
+
+        # Set x-axis and y-axis labels
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+
+        # Set x-axis limits
+        ax.set_xlim(xlims)
+
+        # Set y-axis limits
+        ax.set_ylim(ylims)
+
+        # Add legend
+        ax.legend()
+
+    def drawVolcanoRho(
+            self, ax,
+            rho_df=None,
+            dot_size=1,
+            score_col='score', 
+            pvalue_col='pvalue',
+            xlabel='auto',
+            ylabel='-log10(pvalue)',
+            xlims='auto',
+            ylims='auto',
+            **args
+            ):
+        if rho_df is None:
+            _, _, rho_df = self._prep_data()
+        if xlabel == 'auto':
+            xlabel = self.rho_score_name.replace(':', ': ').replace('_', ' ')
+        
+        self._volcano(ax, rho_df, 
+                      up_hit='resistance_hit', down_hit='sensitivity_hit',
+                      score_col=score_col, pvalue_col=pvalue_col,
+                      xlabel=xlabel, ylabel=ylabel,
+                      dot_size=dot_size, xlims=xlims, ylims=ylims,
+                      **args)
+    
+    def drawVolcanoGamma(
+            self, ax,
+            gamma_df=None,
+            dot_size=1,
+            score_col='score', 
+            pvalue_col='pvalue',
+            xlabel='auto',
+            ylabel='-log10(pvalue)',
+            xlims='auto',
+            ylims='auto',
+            **args
+            ):
+        if gamma_df is None:
+            gamma_df, _, _ = self._prep_data()
+        if xlabel == 'auto':
+            xlabel = self.gamma_score_name.replace(':', ': ').replace('_', ' ')
+        
+        self._volcano(ax, gamma_df, 
+                      up_hit='up_hit', down_hit='essential_hit',
+                      score_col=score_col, pvalue_col=pvalue_col,
+                      xlabel=xlabel, ylabel=ylabel,
+                      dot_size=dot_size, xlims=xlims, ylims=ylims,
+                      **args)
+        
+    def drawVolcanoTau(
+            self, ax,
+            tau_df=None,
+            dot_size=1,
+            score_col='score', 
+            pvalue_col='pvalue',
+            xlabel='auto',
+            ylabel='-log10(pvalue)',
+            xlims='auto',
+            ylims='auto',
+            **args
+            ):
+        if tau_df is None:
+            _, tau_df, _, = self._prep_data()
+        if xlabel == 'auto':
+            xlabel = self.tau_score_name.replace(':', ': ').replace('_', ' ')
+        
+        self._volcano(ax, tau_df, 
+                      up_hit='up_hit', down_hit='down_hit',
+                      score_col=score_col, pvalue_col=pvalue_col,
+                      xlabel=xlabel, ylabel=ylabel,
+                      dot_size=dot_size, xlims=xlims, ylims=ylims,
+                      **args)
+
+    def drawRhoGammaScatter(
+            self, ax,
+            rho_df=None, gamma_df=None,
+            dot_size=1,
+            score_col='score',
+            xlabel='auto',
+            ylabel='auto',
+            xlims='auto',
+            ylims='auto',
+            **args
+            ):
+        #TODO: fix by making a single dataframe with both rho and gamma scores
+        if rho_df is None:
+            _, _, rho_df = self._prep_data()
+        if gamma_df is None:
+            gamma_df, _, _ = self._prep_data()
+
+        if xlabel == 'auto':
+            xlabel = self.rho_score_name.replace(':', ': ').replace('_', ' ')
+        if ylabel == 'auto':
+            ylabel = self.gamma_score_name.replace(':', ': ').replace('_', ' ')
+        
+        # color by rho score labels
+        up_hit = 'resistance_hit'
+        down_hit = 'sensitivity_hit'
+
+        # Scatter plot for each category
+        ax.scatter( rho_df.loc[rho_df['label'] == 'target_non_hit', score_col],
+                    gamma_df.loc[rho_df['label'] == 'target_non_hit', score_col],
+                    alpha=0.1, s=dot_size, c='black', label='target_non_hit',
+                    **args)
+        
+        ax.scatter( rho_df.loc[rho_df['label'] == up_hit, score_col],
+                    gamma_df.loc[rho_df['label'] == up_hit, score_col],
+                    alpha=0.9, s=dot_size, c='#fcae91', label=up_hit,
+                    **args)
+        
+        ax.scatter( rho_df.loc[rho_df['label'] == down_hit, score_col],
+                    gamma_df.loc[rho_df['label'] == down_hit, score_col],
+                    alpha=0.9, s=dot_size, c='#bdd7e7', label=down_hit,
+                    **args)
+        
+        ax.scatter( rho_df.loc[rho_df['label'] == self.ctrl_label, score_col],
+                    gamma_df.loc[rho_df['label'] == self.ctrl_label, score_col],
+                    alpha=0.1, s=dot_size, c='gray', label=self.ctrl_label,
+                    **args)
+        
+        # Set x-axis and y-axis labels
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+
+        # Set x-axis limits
+        ax.set_xlim(xlims)
+        ax.set_ylim(ylims)
+
+        # Add legend
+        ax.legend()
+        
     def label_as_black(self, ax, df_in, label, 
                        x_col='score', y_col='-log10(pvalue)',
                        size=2, size_txt="auto",
@@ -126,25 +302,3 @@ class ScatterPlot:
                              textcolor='black',
                              t_x=t_x, t_y=t_y,
                              **args)
-
-
-class VolcanoPlot(ScatterPlot):
-    def __init__(
-            self, df, up_hit, down_hit, ax,
-            score_col='score', pvalue_col='pvalue',
-            xlabel='phenotype score',
-            ylabel='-log10(pvalue)',
-            ctrl_label='negative_control',
-            dot_size=1,
-            xlims='auto',
-            ylims='auto',
-            **args
-            ):
-        super().__init__(df, up_hit, down_hit, ax,
-                         score_col=score_col, pvalue_col=pvalue_col,
-                         xlabel=xlabel, ylabel=ylabel,
-                         ctrl_label=ctrl_label,
-                         dot_size=dot_size,
-                         xlims=xlims, ylims=ylims,
-                         **args)
-
