0ee04340974e1b1a279b29ced3da3393694b6216
abearab <abea@arcinstitute.org>
2024-08-09 02:32:34 -0700

    mend

--- screenpro/phenoscore/delta.py
+++ screenpro/phenoscore/delta.py
@@ -215,7 +215,6 @@ def getBestTargetByTSS(score_df,target_col,pvalue_col):
     """
     collapse the gene-transcript indices into a single score for a gene by best p-value
     """
-    #TODO: implement this function, see #90
     return score_df.groupby(target_col).apply(
         lambda x: x.loc[x[pvalue_col].idxmin()]
     )
